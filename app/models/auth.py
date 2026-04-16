2229fb63c013efd3ea06253be4a73f71f441c534
Myo Thura Lwin <myothuralwin.mtl@gmail.com>
2026-04-16 21:58:23 +1200

    BE-05 Registration API Endpoint

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,1 +1,12 @@
 from pydantic import BaseModel, EmailStr, field_validator
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password_length(cls, v: str):
+        if len(v.encode("utf-8")) > 72:
+            raise ValueError("Password must be 72 bytes or fewer.")
+        return v
