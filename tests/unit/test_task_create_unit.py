8192d6d6f4ffce5c68b710d6965ba53b4f521401
Chit Toe Nyan <nych001@ess.ais.ac.nz>
2026-04-17 00:29:26 +1200

    BE-09 Create Task API Endpoint - remove duplicate unit test function

--- tests/unit/test_task_create_unit.py
+++ tests/unit/test_task_create_unit.py
@@ -13,14 +13,3 @@ async def test_create_task_sets_default_status_and_owner(unit_request):
     assert response["description"] == "unit + integration"
     assert response["status"] == "To Do"
     assert response["owner_email"] == "owner@example.com"
-
-@pytest.mark.anyio
-async def test_create_task_sets_default_status_and_owner1(unit_request):
-    payload = TaskCreate(title="Write tests", description="unit + integration")
-
-    response = await task_routes.create_task(payload=payload, request=unit_request, user_email="owner@example.com")
-
-    assert response["title"] == "Write tests"
-    assert response["description"] == "unit + integration"
-    assert response["status"] == "To Do"
-    assert response["owner_email"] == "owner@example.com"
